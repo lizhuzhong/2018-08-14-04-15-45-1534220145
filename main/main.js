3f4bee65b2b96556f1d9fad80e24070280728361
lizhuzhong <44048008+lizhuzhong@users.noreply.github.com>
2018-10-12 18:38:03 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,8 @@
 // Write your cade below:
-module.exports = function main() {
-  
+module.exports = function yushu_prompt()
+{
+var a=prompt("请输入第一个数","");
+var b=prompt("请输入第二个数","");
+var n=a%b;
+alert("两个数的余数="+n);
 };
